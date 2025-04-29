20be232989a4263df954b276fe97d1068c5a4ceb
Alireza Aberoumand <maya7192@yahoo.com>
2025-04-28 17:34:46 -0700

    Remove unused Redux dispatch import from MobileNavbar component to streamline code.

--- components/NavBar/MobileNavbar/index.tsx
+++ components/NavBar/MobileNavbar/index.tsx
@@ -8,7 +8,7 @@ import {
   SheetTitle,
   SheetTrigger,
 } from "@/components/ui/sheet";
-import { useAppDispatch, useAppSelector } from "@/redux/hooks";
+import { useAppSelector } from "@/redux/hooks";
 import { SignInButton, SignOutButton, useAuth } from "@clerk/nextjs";
 import {
   BellIcon,
